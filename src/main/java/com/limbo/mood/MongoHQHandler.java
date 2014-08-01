8a8712d1f40792708e509c42804456b7b93accf4
Eran Globen <limbo2k@gmail.com>
2014-08-01 12:57:33 -0700

    Clean up MongoHQHandler. Replaced deprecated methods with new ones (mongo client 2.12)

--- src/main/java/com/limbo/mood/MongoHQHandler.java
+++ src/main/java/com/limbo/mood/MongoHQHandler.java
@@ -10,6 +10,8 @@ import org.eclipse.jetty.server.handler.ContextHandler;
 
 import com.mongodb.DB;
 import com.mongodb.DBCollection;
+import com.mongodb.MongoClient;
+import com.mongodb.MongoClientURI;
 import com.mongodb.MongoException;
 import com.mongodb.MongoURI;
 
@@ -17,22 +19,22 @@ import com.mongodb.MongoURI;
  * Provides access to MongodbHQ service.
  * @author limbo
  *
+ * TODO: better exception handling. Probably just log and fail.
  */
 public class MongoHQHandler {
-
-	private static final DB db = initDB();
+	private static final MongoClientURI mongoURI = initURI();
+	private static final MongoClient mongoClient = initClient(mongoURI);
+	private static final DB db = initDB(mongoClient, mongoURI.getDatabase());
 	
-	private static DB initDB() {
-		DB db_ = null;
+	
+	private static MongoClientURI initURI() {
+		MongoClientURI uri = null;
 		try {
 			Properties props = System.getProperties();
 			URL r = ContextHandler.getCurrentContext().getResource("/WEB-INF/mongohq.properties");
 	   		props.load(r.openStream());
-			MongoURI mongoURI = new MongoURI(props.getProperty("mongohq-url"));
-	   		System.err.println("MongoURI: " + mongoURI.toString());
-	   		
-			db_ = mongoURI.connectDB();
-			db_.authenticate(mongoURI.getUsername(), mongoURI.getPassword());
+			uri = new MongoClientURI(props.getProperty("mongohq-url"));
+	   		System.err.println("MongoURI: " + uri.toString());
 		} catch (MongoException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
@@ -46,8 +48,30 @@ public class MongoHQHandler {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		
+
+		return uri;
+	}
+	
+	private static DB initDB(MongoClient client, String dbName) {
+		DB db_ = null;
+		db_ = mongoClient.getDB(dbName);
+
 		return db_;
+	}
+	private static MongoClient initClient(MongoClientURI uri) {
+		MongoClient client = null;
+		
+		try {	   		
+	   		client = new MongoClient(uri);
+		} catch (MongoException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (UnknownHostException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		return client;
 	}
 	
 	public static DB getDB() {
