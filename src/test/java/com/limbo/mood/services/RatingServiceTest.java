5f24a6c0f3eb4009bed9ad121ca1cf23e6ecd8fa
Eran Globen <limbo2k@gmail.com>
2014-08-03 13:32:00 -0700

    *ServiceTest: Extracted setup and related methods into superclass.

--- src/test/java/com/limbo/mood/services/RatingServiceTest.java
+++ src/test/java/com/limbo/mood/services/RatingServiceTest.java
@@ -4,12 +4,6 @@ import static com.eclipsesource.restfuse.Assert.assertOk;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 
-import java.util.Date;
-
-import org.eclipse.jetty.server.Server;
-import org.eclipse.jetty.webapp.WebAppContext;
-import org.junit.AfterClass;
-import org.junit.Assert;
 import org.junit.BeforeClass;
 import org.junit.Rule;
 import org.junit.runner.RunWith;
@@ -22,120 +16,20 @@ import com.eclipsesource.restfuse.RequestContext;
 import com.eclipsesource.restfuse.Response;
 import com.eclipsesource.restfuse.annotation.Context;
 import com.eclipsesource.restfuse.annotation.HttpTest;
-import com.limbo.mood.MongoHQHandlerTest;
-import com.mongodb.BasicDBObject;
-import com.mongodb.DB;
-import com.mongodb.DBCollection;
-import com.mongodb.DBObject;
 
 // TODO: extract generic test class with all setup code.
 @RunWith(HttpJUnitRunner.class)
-public class RatingServiceTest {
+public class RatingServiceTest extends AbstractServiceTest {
+	@Context
+	public Response response; // will be injected after every request
     
-	public static final String testCollectionName = "mood-ratings-test";
-	public static final String testCollectionPostfix = "-test";
-
-	private static String testID = "moo";
-	private static String testAuthToken = "testAuthTokenValue";
-	private static Server server;
-	
-	private static void startService() throws Exception {
-        String webappDirLocation = "src/main/webapp/";
-        
-        
-        server = new Server(8080);
-        WebAppContext context = new WebAppContext();
-
-        context.setContextPath("/");
-        context.setDescriptor(webappDirLocation + "/WEB-INF/web.xml");
-        System.err.println("DESC: " + context.getDescriptor());
-        context.setResourceBase(webappDirLocation);
-        System.err.println("BASE: " + context.getResourceBase());
-        context.setParentLoaderPriority(true);
-//        root.setClassLoader(this.getClass().getClassLoader());
-        context.setAttribute("collection-postfix", testCollectionPostfix);
-        
-        server.setHandler(context);
-
-        server.start();
-        server.setStopAtShutdown(true);
-        long start = System.currentTimeMillis();
-        while (!server.isStarted() && System.currentTimeMillis() < (start + 5 * 1000)) {
-            //
-        }
-        if (!server.isStarted()) {
-            throw new Exception("Jetty didn't start in time");
-        }	
-        System.err.println("Started!");
-    }
-	
 	@BeforeClass
-	public static void setup() {
-		// start web service
-		try {
-			startService();
-		} catch (Exception e) {
-			Assert.assertNull("failed to start server: " + e.getMessage(), e);
-		}
-		
-        // drop collection
-		DB db = MongoHQHandlerTest.getDB();
-		if (db.getCollection(testCollectionName) != null) {
-			db.getCollection(testCollectionName).drop();
-		}
-		if (db.getCollection(UserServiceTest.testCollectionName) != null) {
-			db.getCollection(UserServiceTest.testCollectionName).drop();
-		}
-		
-		db.createCollection(testCollectionName, null);
-		db.createCollection(UserServiceTest.testCollectionName, null);
-		
+	public static void setupRatingServiceTest() {
+		setup();
 		createTestUserWithAuth();
     	testID = createTestRating();
     	
     	assertNotNull(testID);
-
-	}
-
-	private static void stopServer() throws Exception {
-		server.stop();
-		server.join();
-	}
-	
-	@AfterClass
-	public static void destroy() throws Exception {
-		stopServer();
-	}
-	
-	private static String createTestRating() {
-    	DBCollection testCollection = MongoHQHandlerTest.getCollection(testCollectionName);
-
-    	DBObject newRating = new BasicDBObject();
-    	newRating.put("time", new Date());
-    	newRating.put("rating", 5);
-    	newRating.put("locations", "test");
-    	newRating.put("tags1", "test1");
-    	newRating.put("tags2", "test2");
-    	newRating.put("tags3", "test3");
-    	newRating.put("tags4", null);
-    	newRating.put("person1", "tester1");
-    	newRating.put("person2", null);
-    	newRating.put("person3", "tester2");
-    	newRating.put("person4", "tester3");
-
-    	testCollection.insert(newRating);
-    	return newRating.get("_id").toString();
-	}
-	
-	private static void createTestUserWithAuth() {
-    	DBCollection testCollection = MongoHQHandlerTest.getCollection(UserServiceTest.testCollectionName);
-
-    	DBObject newUser = new BasicDBObject();
-    	newUser.put("email", "tester@test.com");
-    	newUser.put("password", "NieQminDE4Ggcewn98nKl3Jhgq7Smn3dLlQ1MyLPswq7njpt8qwsIP4jQ2MR1nhWTQyNMFkwV19g4tPQSBhNeQ");
-    	newUser.put("authtoken", testAuthToken);
-
-    	testCollection.insert(newUser);
 	}
 
 	@Rule
@@ -149,9 +43,6 @@ public class RatingServiceTest {
 	    return destination;
 	}
 	
-	@Context
-	private Response response; // will be injected after every request
-
 	@HttpTest( method = Method.GET, path = "/services/rating/{id}" )
     public void testGet() throws Exception {
     	
--- src/test/java/com/limbo/mood/services/UserServiceTest.java
+++ src/test/java/com/limbo/mood/services/UserServiceTest.java
@@ -4,10 +4,6 @@ import static com.eclipsesource.restfuse.Assert.assertOk;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 
-import org.eclipse.jetty.server.Server;
-import org.eclipse.jetty.webapp.WebAppContext;
-import org.junit.AfterClass;
-import org.junit.Assert;
 import org.junit.BeforeClass;
 import org.junit.Rule;
 import org.junit.runner.RunWith;
@@ -20,93 +16,20 @@ import com.eclipsesource.restfuse.RequestContext;
 import com.eclipsesource.restfuse.Response;
 import com.eclipsesource.restfuse.annotation.Context;
 import com.eclipsesource.restfuse.annotation.HttpTest;
-import com.limbo.mood.MongoHQHandlerTest;
-import com.mongodb.BasicDBObject;
-import com.mongodb.DB;
-import com.mongodb.DBCollection;
-import com.mongodb.DBObject;
-import com.mongodb.WriteResult;
 
 @RunWith(HttpJUnitRunner.class)
-public class UserServiceTest {
-    
-	public static final String testCollectionName = "mood-users-test";
-	public static final String testCollectionPostfix = "-test";
-	private static String testID = "moo";
-	private static Server server;
-	
-	private static void startService() throws Exception {
-        String webappDirLocation = "src/main/webapp/";
-        
-        
-        server = new Server(8080);
-        WebAppContext context = new WebAppContext();
+public class UserServiceTest extends AbstractServiceTest {
+	@Context
+	public Response response; // will be injected after every request
 
-        context.setContextPath("/");
-        context.setDescriptor(webappDirLocation + "/WEB-INF/web.xml");
-        System.err.println("DESC: " + context.getDescriptor());
-        context.setResourceBase(webappDirLocation);
-        System.err.println("BASE: " + context.getResourceBase());
-        context.setParentLoaderPriority(true);
-//        root.setClassLoader(this.getClass().getClassLoader());
-        context.setAttribute("collection-postfix", testCollectionPostfix);
-        
-        server.setHandler(context);
-
-        server.start();
-        server.setStopAtShutdown(true);
-        long start = System.currentTimeMillis();
-        while (!server.isStarted() && System.currentTimeMillis() < (start + 5 * 1000)) {
-            //
-        }
-        if (!server.isStarted()) {
-            throw new Exception("Jetty didn't start in time");
-        }	
-        System.err.println("Started!");
-    }
-	
 	@BeforeClass
-	public static void setup() {
-		// start web service
-		try {
-			startService();
-		} catch (Exception e) {
-			Assert.assertNull("failed to start server: " + e.getMessage(), e);
-		}
-		
-        // drop collection
-		DB db = MongoHQHandlerTest.getDB();
-		if (db.getCollection(testCollectionName) != null) {
-			db.getCollection(testCollectionName).drop();
-		}
-		
-		db.createCollection(testCollectionName, null);
+	public static void setupUserServiceTest() {
+		setup();
 		
     	testID = createTestUser();
     	assertNotNull(testID);
 	}
 
-	private static void stopServer() throws Exception {
-		server.stop();
-		server.join();
-	}
-	
-	@AfterClass
-	public static void destroy() throws Exception {
-		stopServer();
-	}
-
-	private static String createTestUser() {
-    	DBCollection testCollection = MongoHQHandlerTest.getCollection(testCollectionName);
-
-    	DBObject newUser = new BasicDBObject();
-    	newUser.put("email", "tester@test.com");
-    	newUser.put("password", "NieQminDE4Ggcewn98nKl3Jhgq7Smn3dLlQ1MyLPswq7njpt8qwsIP4jQ2MR1nhWTQyNMFkwV19g4tPQSBhNeQ");
-
-    	WriteResult result = testCollection.insert(newUser);
-    	return newUser.get("_id").toString();
-	}
-	
 	@Rule
 	public Destination destination = getDestination();
 	
@@ -119,9 +42,6 @@ public class UserServiceTest {
 	    return destination;
 	}
 	
-	@Context
-	private Response response; // will be injected after every request
-
 	@HttpTest( method = Method.GET, path = "/services/user/{id}" )
     public void testGet() throws Exception {
     	
